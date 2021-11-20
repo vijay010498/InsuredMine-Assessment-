f8cb4496cc88ce74aa3683afcb8e96d1017c3a6e
vijay <vijay.sudeshkumar@novade.net>
2021-11-20 22:23:31 +0530

    added policy carrier model

---
+++ src/models/PolicyCarrier.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+interface policyCarrierAttrs {
+  companyName: String;
+}
+
+interface policyCarrierModel extends mongoose.Model<policyCarrierDoc> {
+  build(attrs: policyCarrierAttrs): policyCarrierDoc;
+}
+interface policyCarrierDoc extends mongoose.Document {
+  companyName: String;
+}
+
+const PolicyCarrierSchema = new mongoose.Schema(
+  {
+    companyName: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        const createdAtISO = ret.createdAt;
+        delete ret.createdAt;
+        ret.createdAt = createdAtISO.getTime();
+        const updatedAtISO = ret.updatedAt;
+        delete ret.updatedAt;
+        ret.updatedAt = updatedAtISO.getTime();
+      },
+    },
+  }
+);
+
+PolicyCarrierSchema.statics.build = (attrs: policyCarrierAttrs) => {
+  return new PolicyCarrier(attrs);
+};
+
+const PolicyCarrier = mongoose.model<policyCarrierDoc, policyCarrierModel>(
+  "PolicyCarrier",
+  PolicyCarrierSchema
+);
+export { PolicyCarrier };
